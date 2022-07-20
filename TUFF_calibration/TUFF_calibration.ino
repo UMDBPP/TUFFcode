f2e23057ea489c30011e192fb396c2183162dac0
GidntSquia <jaxondlee@gmail.com>
2022-07-20 12:31:11 -0400

    Implement spreadsheet calibration method

--- TUFF_calibration/TUFF_calibration.ino
+++ TUFF_calibration/TUFF_calibration.ino
@@ -1,21 +1,26 @@
 /*===========================================================================
- * T.U.F.F (Tension Under Flight Forces) UMD BPP 
+ * T.U.F.F (Tension Under Flight Forces) UMD BPP
  * Sensor calibration code
  * By Jeremy Kuznetsov and Jaxon Lee
- * 
+ *
  * This code identifies the practical "scale" for the tension load cell.
  * The scale converts the load cell reading to human-readable measurements (lbs).
  * Specifically, the conversion works as follows:
  *  load_cell_measurement / SCALE = human_readable_measurement
- * 
+ *
  * Procedure:
  *  1. Allow the sensor 5 seconds to "zero" itself before applying any weight
- *  2. Put known weight (~5 lbs) on the load cell
+ *  2. Put known weight (~2 lbs) on the load cell
  *  3. Write down the value that results. Many numbers will pop out, so use
  *     your best judgement to find a general average
- *  4. Assign this value to the "LOADCELL_DIVIDER" variable in the in-flight
- *     source code (yes, you need to hard code the value in).
- *  5. Profit :)
+ *  4. Repeat steps 2. and 3. with increasing weight until 20 lbs is reached
+ *  5. Input values into a spreadsheet. Graph the tension as "x" and the
+ *     known weight as "y". Create a line of best fit and note the "m" in "mx+b".
+ *     It should be a low value between 0 and  1 (ex. 0.00003).
+ *  6. Assign the INVERSE (ex. 0.00003 ^-1 = 33,333) of this value to the 
+ *     "LOADCELL_DIVIDER" variable in the in-flight source code (yes, you need 
+ *     to hard code the value in).
+ *  7. Profit :)
  ===========================================================================*/
 
 // Libraries
@@ -65,15 +70,11 @@ void setup() {
  * Spits out a value needed to calibrate the tension load cell.
  ===========================================================================*/
 void loop() {
-  // The weight in POUNDS that you put on the load cell for calibration.
-  // Note the "f" for float.
-  float known_weight = 8.380674356f;
 
-  // The divider scale required to convert load cell units to human readable units.
-  // This averages the load cell's value across 100 readings.
-  float divider_scale = loadcell.get_units(100) / known_weight;
+  // This averages the load cell's value across 250 readings.
+  float tension = loadcell.get_value(250);
 
-  Serial.print("Divider scale: ");
-  Serial.println(divider_scale);
+  Serial.print("Tension: ");
+  Serial.println(tension);
   delay(100);
 }
